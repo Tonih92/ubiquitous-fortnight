3eeabe95f9873098d2f1fb692f4db4d6eb6210d0
Toni Huusko <toni.huusko92@gmail.com>
2024-11-28 17:43:25 +0200

    Updated app

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // app.js
 import { Hono } from "https://deno.land/x/hono/mod.ts";
+import { loginUser } from "./routes/login.js"; // Import login logic 
 import { registerUser } from "./routes/register.js"; // Import register logic
+import { serveStatic } from "https://deno.land/x/hono/middleware.ts";
+
 const app = new Hono();
+
+// Serve static files from the /static directory
+app.use('/static/*', serveStatic({ root: '.' }));
 
 // Serve the registration form
 app.get('/register', async (c) => {
@@ -10,6 +16,14 @@ app.get('/register', async (c) => {
 
 // Route for user registration (POST request)
 app.post('/register', registerUser);
+
+// Serve login page
+app.get('/login', async (c) => {
+  return c.html(await Deno.readTextFile('./views/login.html')); // Use the login.html file
+});
+// Handle user login
+app.post('/login', loginUser);
+
 
 Deno.serve(app.fetch);
 
